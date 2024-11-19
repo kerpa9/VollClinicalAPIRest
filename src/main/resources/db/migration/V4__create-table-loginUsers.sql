5e129a3374582602176c5ea2dbb8640a293d1c86
Kevin Reyes <kerpa9@gmail.com>
2024-11-19 11:45:06 -0500

    Create migration table login

---
+++ src/main/resources/db/migration/V4__create-table-loginUsers.sql
@@ -0,0 +1,10 @@
+
+create table loginUsers(
+        id bigint not null auto_increment,
+        login varchar(100) not null,
+        password varchar(100) not null,
+    
+
+        primary key(id)
+
+);
