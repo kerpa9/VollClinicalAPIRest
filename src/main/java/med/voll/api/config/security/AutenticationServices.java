47e02488c4f725bd8399dbe2d085d33e234f567c
Kevin Reyes <kerpa9@gmail.com>
2024-11-19 12:14:47 -0500

    Initial encrypt pass

---
+++ src/main/java/med/voll/api/config/security/AutenticationServices.java
@@ -0,0 +1,23 @@
+package med.voll.api.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import med.voll.api.repository.LoginRepository;
+
+@Service
+public class AutenticationServices implements UserDetailsService {
+
+    // Use methods special JPA
+    @Autowired
+    private LoginRepository loginRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return loginRepository.findByUsername(username);
+    }
+
+}
---
+++ src/main/java/med/voll/api/repository/LoginRepository.java
@@ -0,0 +1,12 @@
+package med.voll.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import med.voll.api.domain.users.LoginUsers;
+
+public interface LoginRepository extends JpaRepository<LoginUsers, Long> {
+
+    UserDetails findByUsername(String username);
+
+}
