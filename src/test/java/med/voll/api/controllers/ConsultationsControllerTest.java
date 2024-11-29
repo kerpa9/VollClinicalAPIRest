98f0f0ad8be5b4edfde87fc3341f9dbbd8c45a98
Kevin Reyes <kerpa9@gmail.com>
2024-11-29 18:07:51 -0500

    Initial unit test (Method reservation)

---
+++ src/test/java/med/voll/api/controllers/ConsultationsControllerTest.java
@@ -0,0 +1,16 @@
+package med.voll.api.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConsultationsControllerTest {
+
+    private MockMvc mockMvc;
+
+
+    @Test
+    void testReservation1() {
+
+    }
+}
