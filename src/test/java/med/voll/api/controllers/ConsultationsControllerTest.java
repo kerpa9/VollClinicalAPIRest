7154e37f13c95e9b2faadff2f09b215deaa0bd34
Kevin Reyes <kerpa9@gmail.com>
2024-11-30 12:47:29 -0500

    Create integration test (Mock) consultation controller

--- src/test/java/med/voll/api/controllers/ConsultationsControllerTest.java
+++ src/test/java/med/voll/api/controllers/ConsultationsControllerTest.java
@@ -1,16 +1,32 @@
 package med.voll.api.controllers;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ConsultationsControllerTest {
 
-    private MockMvc mockMvc;
-
+    @Autowired
+    private MockMvc mvc;
 
     @Test
-    void testReservation1() {
+    @DisplayName("Return http 400 ")
+    @WithMockUser
+    void testReservation1() throws Exception {
+
+        var response = mvc.perform(MockMvcRequestBuilders.post("/api/v1/consultations")).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+
 
     }
 }
